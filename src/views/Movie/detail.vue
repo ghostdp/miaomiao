3353e2335e001680e60a4afcf62de5d3e7166442
du <new666@qq.com>
2019-03-10 12:02:15 +0800

    bug clear

--- src/views/Movie/detail.vue
+++ src/views/Movie/detail.vue
@@ -37,7 +37,7 @@
 				</ul>
 			</div> -->
             <div class="detail_list">
-				<div class="detail_list_bg"></div>
+				<div class="detail_list_bg" :style="{ 'background-image' : 'url('+ detailMovie.img.replace(/w\.h/,'148.208') +')' }"></div>
 				<div class="detail_list_filter"></div>
 				<div class="detail_list_content">
 					<div class="detail_list_img">
@@ -121,7 +121,7 @@ export default {
 }
 #content.contentDetail{ display: block; margin-bottom:0;}
 #content .detail_list{ height:200px; width:100%; position: relative; overflow: hidden;}
-.detail_list .detail_list_bg{ width:100%; height:100%; background: url(/images/movie_1.jpg) 0 40%; filter: blur(20px); background-size:cover; position: absolute; left: 0; top: 0;}
+.detail_list .detail_list_bg{ width:100%; height:100%; background: 0 40%; filter: blur(20px); background-size:cover; position: absolute; left: 0; top: 0;}
 .detail_list .detail_list_filter{ width:100%; height:100%; position: absolute;background-color: #40454d;opacity: .55; position: absolute; left: 0; top: 0; z-index: 1;}
 .detail_list .detail_list_content{ display: flex; width:100%; height:100%; position: absolute; left: 0; top: 0; z-index: 2;}
 .detail_list .detail_list_img{ width:108px; height: 150px; border: solid 1px #f0f2f3; margin:20px;}
